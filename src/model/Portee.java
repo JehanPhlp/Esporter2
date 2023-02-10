caf5025b515155c38341de24d541989b3649d7db
JehanPhlp <93180306+JehanPhlp@users.noreply.github.com>
2023-02-10 19:00:31 +0100

    yesss

---
+++ src/model/Portee.java
@@ -0,0 +1,36 @@
+
+package model;
+
+public enum Portee {
+	LOCAL("Local"), 
+	NATIONAL("National"),
+	INTERNATIONAL("International");
+	
+	private final String name;
+
+	private Portee(String s) {
+		name = s;
+	}
+	
+	public static String[] toStrings() {
+		String[] portees = {LOCAL.getName(), NATIONAL.getName(), INTERNATIONAL.getName()};
+		return portees;
+	}
+	
+	public static Portee stringToPortee(String str) {
+		switch(str) {
+			case "Local":
+				return Portee.LOCAL;
+			case "National":
+				return Portee.NATIONAL;
+			case "International" :
+				return Portee.INTERNATIONAL;
+			default :
+				return null;
+		}
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -8,4 +8,6 @@
 module Esporter2 {
 	requires java.sql;
 	requires com.oracle.database.jdbc;
+	requires java.desktop;
+	requires Esporter;
 }
